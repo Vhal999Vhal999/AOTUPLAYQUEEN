760a04af7f4919d66207713eb79d3d46bddd730e
MADARA X CODER <ragini.19854@gmail.com>
2026-07-08 18:17:43 +0530

    Update Youtube.py

--- SHUKLAMUSIC/platforms/Youtube.py
+++ SHUKLAMUSIC/platforms/Youtube.py
@@ -17,7 +17,7 @@ import aiohttp
 
 API_URL = os.environ.get("API_URL", "https://api01.shrutibots.site")
 
-API_KEY = os.environ.get("API_KEY", "ShrutiBotskETPIMNljdHR0VKMIgbz") ## Get This API KEY FROM: @SHRUTIAPIBOT 
+API_KEY = os.environ.get("API_KEY", "ShrutiBots6E6WORLVHvbE873Ptuv7") ## Get This API KEY FROM: @SHRUTIAPIBOT 
 
 DOWNLOAD_DIR = "downloads"
 
