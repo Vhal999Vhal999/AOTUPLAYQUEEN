587b4f2b9f6ac0342c6ce8d906e2435c44f13a3e
- ᴋɪʀᴛɪ ʙоᴛѕ [ 🇮🇳 <kritikumari8665@gmail.com>
2026-07-19 15:45:52 +0530

    Change user filter for 'done' command

--- SHUKLAMUSIC/plugins/tools/reload.py
+++ SHUKLAMUSIC/plugins/tools/reload.py
@@ -116,7 +116,7 @@ async def restartbot(client, message: Message, _):
 @app.on_message(
     filters.command("done")
     & filters.private
-    & filters.user(6919199044)
+    & filters.user(8857291657)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
